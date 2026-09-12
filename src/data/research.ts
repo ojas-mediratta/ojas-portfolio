269dd6cdc8ba63f135aefa9a0474cfb61e5de9dd
Ojas Mediratta <ojas.mediratta@live.com>
2026-09-11 20:38:13 -0700

    WTUMI accepted

--- src/data/research.ts
+++ src/data/research.ts
@@ -36,7 +36,7 @@ export const RESEARCH: Research[] = [
     date: "2026",
     type: "Paper",
     authors: ["Jaehwi Jang*", "Zhaoyuan Gu*", "Alfred Cueva", "Zimeng Chai", "Junjie Sheng", "Thong Nguyen", "Himank Galundia", "Yifan Wu", "Huishu Xue", "Isaac Legene", "Ojas Mediratta", "Davin Doan", "Andrew Collins", "Sarah Sadegh", "KyoungMok Kim", "Rishita Dhalbisoi", "Zun Chen", "Ye Zhao"],
-    status: "Under Review",
+    status: "Accepted",
     href: "https://wt-umi.github.io/WTUMI/", 
     paper: "https://arxiv.org/abs/2606.13232",
     thumb: "media/tactile-sensing/tactile-sensing-research_thumb.png",
